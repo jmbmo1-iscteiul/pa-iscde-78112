22141eca98bd8e3f20e88f3edd8dc0e78470cdfd
josemagrinho <54418462+josemagrinho@users.noreply.github.com>
2020-01-09 02:32:57 +0000

    Update Activator.java

--- CodeGeneratorPlugin/src/pt/iscde/codegenerator/ext/Activator.java
+++ CodeGeneratorPlugin/src/pt/iscde/codegenerator/ext/Activator.java
@@ -8,7 +8,7 @@ public class Activator implements BundleActivator {
 	private static BundleContext context;
 
 	static BundleContext getContext() {
-		return context;
+		return context ;
 	}
 
 	public void start(BundleContext bundleContext) throws Exception {
